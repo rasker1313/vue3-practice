b324d4a2a73a3656c65f8fc3288e1b6842f0b32b
arty1312@gmail.com <Travoman./#1313>
2023-07-14 12:05:02 +0300

    note-list add vuex

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faChevronUp, faPencilSquare, faTrash, faCheck } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import router from '@/router'
+import { noteStore } from '@/store/note-list'
 
 library.add(faChevronUp, faPencilSquare, faTrash, faCheck);
 
 createApp(App)
     .component("font-awesome-icon", FontAwesomeIcon)
-    .use(router)
+    .use(router, noteStore)
     .mount('#app')
---
+++ src/store/note-list.js
@@ -0,0 +1,5 @@
+import { createStore } from 'vuex'
+
+export const noteStore = createStore({
+
+})
